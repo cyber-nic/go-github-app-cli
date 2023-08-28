0b6020baae2010665c8503951f3c4821d6eb9bfd
Nicolas Delorme <nic@4ks.io>
2023-08-28 08:09:11 +0100

    cli commands

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-}	
+	app := &cli.App{
+		Name:  "AuditLab",
+		Usage: "tool for auditing",
+		Commands: []*cli.Command{
+			{
+				Name:  "whoami",
+				Usage: "github user identification",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("authenticated")
+					return nil
+				},
+			},
+			{
+				Name:  "login",
+				Usage: "github login",
+				Action: func(cCtx *cli.Context) error {
+					fmt.Println("logging in ", cCtx.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
