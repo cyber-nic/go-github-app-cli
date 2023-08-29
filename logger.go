ce8dc22d84f13ae3d4b9a480e9eacc819dec4dee
Nicolas Delorme <nic@4ks.io>
2023-08-29 14:32:44 +0100

    complete access token request

--- logger.go
+++ logger.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"net/http"
+	"net/http/httputil"
 	"os"
 
 	"github.com/go-kit/log"
@@ -35,4 +38,22 @@ func loggerFromContext(ctx context.Context) log.Logger {
 	w := log.NewSyncWriter(os.Stderr)
 	logger := log.NewLogfmtLogger(w)
 	return logger
+}
+
+// https://www.jvt.me/posts/2023/03/11/go-debug-http/
+
+type loggingTransport struct{}
+
+func (s *loggingTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	bytes, _ := httputil.DumpRequestOut(r, true)
+
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	// err is returned after dumping the response
+
+	respBytes, _ := httputil.DumpResponse(resp, true)
+	bytes = append(bytes, respBytes...)
+
+	fmt.Printf("%s\n", bytes)
+
+	return resp, err
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -15,18 +16,27 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-func main() {
-	var l *log.Logger
-	var llvl string
+type LOG_LEVEL struct{}
+type CLIENT_ID struct{}
+type CLIENT_SECRET struct{}
 
-	CLIENT_ID := os.Getenv("GITHUB_CLIENT_ID")
-	SECRET_ID := os.Getenv("GITHUB_CLIENT_SECRET")
+func main() {
+	var l log.Logger
+	var llvl string
+	ctx := context.Background()
 
 	app := &cli.App{
 		Name:  "AuditLab",
 		Usage: "tool for auditing",
 		Before: func(cCtx *cli.Context) error {
-			l = newLogger(llvl)
+			// add logger and log level to context
+			ctx = contextWithLogger(ctx, newLogger(llvl))
+			ctx = context.WithValue(ctx, LOG_LEVEL{},  llvl)
+			// add github creds to context
+			ctx = context.WithValue(ctx, CLIENT_ID{},  os.Getenv("GITHUB_CLIENT_ID"))
+			ctx = context.WithValue(ctx, CLIENT_SECRET{},  os.Getenv("GITHUB_CLIENT_SECRET"))
+
+			l = loggerFromContext(ctx)
 			return nil
 		},
 		Flags: []cli.Flag{
@@ -34,7 +44,7 @@ func main() {
 				Name:        "logLevel",
 				Aliases:     []string{"log"},
 				Usage:       "debug, info, warn, error",
-				Value:       "info",
+				Value:       "warn",
 				Destination: &llvl,
 				EnvVars:     []string{"LOG_LEVEL"},
 			},
@@ -44,7 +54,8 @@ func main() {
 				Name:  "whoami",
 				Usage: "github user identification",
 				Action: func(cCtx *cli.Context) error {
-					level.Debug(*l).Log("msg", "whoami")
+
+					level.Info(l).Log("msg", "whoami")
 					return nil
 				},
 			},
@@ -52,8 +63,8 @@ func main() {
 				Name:  "login",
 				Usage: "github login",
 				Action: func(cCtx *cli.Context) error {
-					level.Debug(*l).Log("msg", "login", "client id", CLIENT_ID)
-					login(l, CLIENT_ID, SECRET_ID)
+					level.Info(l).Log("msg", "login", "client id", ctx.Value("client_id"))
+					login(ctx)
 					return nil
 				},
 			},
@@ -61,7 +72,7 @@ func main() {
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		level.Error(*l).Log("msg", "fatal", "error", err)
+		level.Error(l).Log("msg", "fatal", "error", err)
 	}
 }
 
@@ -85,8 +96,9 @@ type RequestTokenResponse struct {
 
 // postForm function is a generic allowing posting of form data to a given URI and returning
 // the correct response
-func postForm[V DeviceCodeResponse | RequestTokenResponse](l *log.Logger, uri string, form url.Values) (V, error) {
+func postForm[V DeviceCodeResponse | RequestTokenResponse](ctx context.Context, uri string, form url.Values) (V, error) {
 	var r V
+	l := loggerFromContext(ctx)
 
 	// Create a new request with the POST method.
 	req, err := http.NewRequest("POST", uri, strings.NewReader(form.Encode()))
@@ -98,11 +110,15 @@ func postForm[V DeviceCodeResponse | RequestTokenResponse](l *log.Logger, uri st
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Accept", "application/json")
 
-	// todo: toggle debugging
-	client := http.Client{
-		Transport: &loggingTransport{},
+	// toggle debugging
+	var client http.Client
+	if ctx.Value(LOG_LEVEL{}) == "debug" {
+		client = http.Client{
+			Transport: &loggingTransport{},
+		}
+	} else {
+		client = http.Client{}
 	}
-	// client := http.Client{}
 
 	// Send the request.
 	resp, err := client.Do(req)
@@ -111,7 +127,7 @@ func postForm[V DeviceCodeResponse | RequestTokenResponse](l *log.Logger, uri st
 	}
 	defer resp.Body.Close()
 
-	level.Debug(*l).Log("msg", "request Device Code", "status_code", resp.StatusCode)
+	level.Info(l).Log("msg", "request Device Code", "status_code", resp.StatusCode)
 
 	// Check the response status code.
 	if resp.StatusCode != http.StatusOK {
@@ -130,7 +146,7 @@ func postForm[V DeviceCodeResponse | RequestTokenResponse](l *log.Logger, uri st
 }
 
 // requestDeviceCode function performs a POST request to https://github.com/login/device/code
-func requestDeviceCode(l *log.Logger, clientID, secretID string) (DeviceCodeResponse, error) {
+func requestDeviceCode(ctx context.Context, clientID, secretID string) (DeviceCodeResponse, error) {
 	var r DeviceCodeResponse
 	uri := "https://github.com/login/device/code"
 
@@ -139,7 +155,7 @@ func requestDeviceCode(l *log.Logger, clientID, secretID string) (DeviceCodeResp
 	form.Add("client_id", clientID)
 	form.Add("secret_id", secretID)
 
-	r, err := postForm[DeviceCodeResponse](l, uri, form)
+	r, err := postForm[DeviceCodeResponse](ctx, uri, form)
 	if err != nil {
 		return r, err
 	}
@@ -148,7 +164,7 @@ func requestDeviceCode(l *log.Logger, clientID, secretID string) (DeviceCodeResp
 }
 
 // requestToken function performs a POST request to https://github.com/login/oauth/access_token
-func requestToken(l *log.Logger, clientID, deviceCode string) (RequestTokenResponse, error) {
+func requestToken(ctx context.Context, clientID, deviceCode string) (RequestTokenResponse, error) {
 	var r RequestTokenResponse
 	uri := "https://github.com/login/oauth/access_token"
 
@@ -158,7 +174,7 @@ func requestToken(l *log.Logger, clientID, deviceCode string) (RequestTokenRespo
 	form.Add("device_code", deviceCode)
 	form.Add("grant_type", "urn:ietf:params:oauth:grant-type:device_code")
 
-	r, err := postForm[RequestTokenResponse](l, uri, form)
+	r, err := postForm[RequestTokenResponse](ctx, uri, form)
 	if err != nil {
 		return r, err
 	}
@@ -167,61 +183,83 @@ func requestToken(l *log.Logger, clientID, deviceCode string) (RequestTokenRespo
 }
 
 // wait function sleep for specified interval
-func wait(l *log.Logger, interval int) {
-	// Wait, then poll again.
+func wait(l log.Logger, interval int) {
+	level.Info(l).Log("msg", "sleep", "interval", interval, "unit", "seconds")
 	time.Sleep(time.Duration(interval) * time.Second)
-	// Printed after sleep is over
-	level.Debug(*l).Log("msg", "Rest is good")
 }
 
 // pollForToken function polls https://github.com/login/oauth/access_token at the specified interval
 // until GitHub responds with an access_token parameter instead of an error parameter.
 // Then, it writes the user access token to a file and restricts the permissions on the file.
-func pollForToken(l *log.Logger, clientID, deviceCode string, interval int) {
-Loop:
-	for {
-		i := interval
-		t, err := requestToken(l, clientID, deviceCode)
+func pollForToken(ctx context.Context, clientID, deviceCode string, interval int) error {
+	l := loggerFromContext(ctx)
+	i := interval
+	var t RequestTokenResponse
 
+	Loop:
+	for {
+		t, err := requestToken(ctx, clientID, deviceCode)
+		if err != nil {
+			return err
+		}
+
+		if t.Error != "" {
+			level.Info(l).Log("msg", "poll", "status", "error", "error", t.Error, "error_description", t.ErrorDescription, "interval", t.Interval)
+		} else {
+			level.Info(l).Log("msg", "poll", "status", "success", "access_token", t.AccessToken)
+		}
+
+		// update interval if available in response
 		value := reflect.ValueOf(t)
 		field := value.FieldByName("Interval")
-		if field.IsValid() {
+		if field.IsValid() && !field.IsZero() {
 			i = t.Interval
 		}
 
-		level.Debug(*l).Log("msg", "ping")
-		time.Sleep(time.Duration(i) * time.Second)
-		level.Debug(*l).Log("msg", "pong")
-
-		if err == nil {
-			level.Debug(*l).Log("msg", "poll for token", "access_token", t.AccessToken)
-			fmt.Println("Login successful.")
-			return
-		}
-
-		level.Debug(*l).Log("msg", "poll for token", "error", err)
-		switch err {
-		case fmt.Errorf("authorization_pending"):
+		switch t.Error {
+		case "authorization_pending":
 			// The user has not yet entered the code.
 			wait(l, i)
-			break Loop
-		case fmt.Errorf("slow_down"):
+		case "slow_down":
 			// The app polled too fast.
 			// Wait for the interval plus 5 seconds, then poll again.
 			wait(l, i+5)
-			break Loop
-		case fmt.Errorf("expired_token"):
+		case "expired_token":
 			// The `device_code` expired, and the process needs to restart.
-			level.Info(*l).Log("msg", "The device code has expired. Please run `login` again.", "error", err)
-			return
-		case fmt.Errorf("access_denied"):
+			return fmt.Errorf(t.Error)
+		case "access_denied":
 			// The user cancelled the process. Stop polling.
-			level.Info(*l).Log("msg", "Login cancelled by user.", "error", err)
-			return
+			return fmt.Errorf(t.Error)
 		default:
-			level.Info(*l).Log("msg", "unknown failure", "error", err)
+			break Loop
 		}
 	}
+
+	// get home dir
+	home, err := os.UserHomeDir()
+	if err != nil {
+		level.Error(l).Log("msg", "failed to get home directory", "error", err)
+		panic(err)
+	}
+
+	// create out dir
+	d := home + "/.github"
+	err = os.MkdirAll(d, 0744)
+	if err !=	nil {
+		level.Error(l).Log("msg", "failed to write access_token to file", "directory", d, "error", err)
+		panic(err)
+	}
+
+	// write access token
+	filename := d + "/credentials"
+	err = os.WriteFile(filename, []byte(t.AccessToken), 0644)
+	if err !=	nil {
+		level.Error(l).Log("msg", "failed to write access token to file", "filename", filename, "error", err)
+		panic(err)
+	}
+
+	fmt.Printf("Successfully authenticated!")
+	return nil
 }
 
 // login functionc alls the request_device_code function and gets the verification_uri, user_code, device_code,
@@ -229,19 +267,30 @@ Loop:
 // It prompts users to enter the user_code from the previous step.
 // It calls the poll_for_token to poll GitHub for an access token.
 // It lets the user know that authentication was successful.
-func login(l *log.Logger, clientID, secretID string) {
+func login(ctx context.Context) {
+	l := loggerFromContext(ctx)
+	c := ctx.Value(CLIENT_ID{}).(string)
+	s := ctx.Value(CLIENT_SECRET{}).(string)
+
 	// get device code
-	r, err := requestDeviceCode(l, clientID, secretID)
+	r, err := requestDeviceCode(ctx, c, s)
 	if err != nil {
 		panic(err)
 	}
 
-	level.Debug(*l).Log("msg", "response", "device_code", r.DeviceCode, "user_code", r.UserCode, "verification_uri", r.VerificationURI)
+	level.Info(l).Log("msg", "response", "device_code", r.DeviceCode, "user_code", r.UserCode, "verification_uri", r.VerificationURI)
 
+	// provide user instructions
 	fmt.Printf("Visit: %s\n", r.VerificationURI)
 	fmt.Printf("Provide code: %s\n", r.UserCode)
 
+	// prompt user to continue
 	var userConfirm string
 	fmt.Scanf("%s", &userConfirm)
-	pollForToken(l, clientID, r.DeviceCode, r.Interval)
+
+	// poll for access token
+	if err = pollForToken(ctx, c, r.DeviceCode, r.Interval); err != nil {
+		level.Error(l).Log("msg", "failed to retrieve access token", "error", err)
+	}
+
 }
